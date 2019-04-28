14f2ba95227970819a978a0fc413477c03132f94
Josh5k <jking57@academic.rrc.ca>
2019-04-28 16:17:16 -0500

    Deploy

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Calculator
-
-An Apple inspired React Calculator. A Live demo can be found here - https://josh5k.github.io/react-calculator/
---
+++ precache-manifest.62652baec5e557e95cecf46949e4ee64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "376681da843dc31e804f",
+    "url": "/react-calculator/static/js/main.4e5348d6.chunk.js"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/react-calculator/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "376681da843dc31e804f",
+    "url": "/react-calculator/static/css/main.cb64a48c.chunk.css"
+  },
+  {
+    "revision": "cd9539fcf5d863bfe1de8520adfd638e",
+    "url": "/react-calculator/index.html"
+  }
+];
